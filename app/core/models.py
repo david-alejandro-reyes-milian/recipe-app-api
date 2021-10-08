1ced0682762288e8421ec2180b4e31526debde14
David Alejandro Reyes Milian <davidreyesmilian91@gmail.com>
2021-10-08 15:58:10 +0200

    create superuser

--- app/core/models.py
+++ app/core/models.py
@@ -4,9 +4,21 @@ from django.db import models
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
-        user = self.model(email=email, **extra_fields)
+    def create_user(self, email: str, password: str = None, **extra_fields):
+        if not email:
+            raise ValueError("Users most have email address")
+        if not password:
+            raise ValueError("Users most have password")
+
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email: str, password: str):
+        user = self.create_user(email, password)
+        user.is_superuser = True
+        user.is_staff = True
         user.save()
         return user
 
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -2,14 +2,40 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from django.test import TestCase
 
+user_model: User = get_user_model()
+
 
 class ModelTests(TestCase):
     def test_create_user_with_email_successful(self):
         email = "t@emamil"
         pwd = "pass"
-        user: User = get_user_model().objects.create_user(
+        user: User = user_model.objects.create_user(
             email=email,
             password=pwd
         )
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(pwd))
+
+    def test_new_user_normalize(self):
+        email = 'test@Loasdsd.com'
+        user = user_model.objects.create_user(email, 'pass')
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        with self.assertRaises(ValueError):
+            user_model.objects.create_user(None, 'pass')
+
+    def test_new_user_invalid_pass(self):
+        with self.assertRaises(ValueError):
+            user_model.objects.create_user(email="email@a.c", password=None)
+
+    def test_create_new_superuser(self):
+        email = "t@emamil"
+        pwd = "pass"
+        user: User = user_model.objects.create_superuser(
+            email=email,
+            password=pwd
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
