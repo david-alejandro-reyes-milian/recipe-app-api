9bb6832102a2bbaadfe7a3df4a97b7fa6bf1f763
David Alejandro Reyes Milian <davidreyesmilian91@gmail.com>
2021-10-14 21:39:10 +0200

    add custom user

--- app/core/admin.py
+++ app/core/admin.py
@@ -2,11 +2,18 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib import admin
 
 from core import models
+from django.utils.translation import gettext as _
 
 
 class UserAdmin(BaseUserAdmin):
     ordering = ['id']
     list_display = ['email', 'name']
+    fieldsets = [
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('name',)}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        (_('Important Dates'), {'fields': ('last_login',)})
+    ]
 
 
 admin.site.register(models.User, UserAdmin)
--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -26,5 +26,12 @@ class AdminSiteTests(TestCase):
     def test_users_listed(self):
         url = reverse('admin:core_user_changelist')
         res = self.client.get(url)
+
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.name)
+
+    def test_user_change_page(self):
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
