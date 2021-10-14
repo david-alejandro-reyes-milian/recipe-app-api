6ec51ad66bffab3a1af1211d3b9a71132d4877c7
David Alejandro Reyes Milian <davidreyesmilian91@gmail.com>
2021-10-14 23:26:04 +0200

    add user test

---
+++ app/user/tests/test_user_api.py
@@ -0,0 +1,33 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.test import APIClient
+
+from core.models import User as UserModel
+
+CREATE_URL = reverse('user:create')
+User: UserModel = get_user_model()
+
+
+def create_user(**param) -> User:
+    return User.objects.create(**param)
+
+
+class PublicUserApiTests(TestCase):
+    def setUp(self) -> None:
+        self.client = APIClient()
+
+    def test_create_valid_user_success(self):
+        payload = {
+            'email': 'test@gmail.com',
+            'password': 'test@gmail.com',
+            'name': 'Test Name'
+        }
+        res: Response = self.client.post(CREATE_URL, payload)
+        self.assertTrue(res.status_code, status.HTTP_201_CREATED)
+        user = User.objects.get(**res.data)
+
+        self.assertEqual(user.check_password(payload['password']))
+        self.assertNotIn('password', res.data)
