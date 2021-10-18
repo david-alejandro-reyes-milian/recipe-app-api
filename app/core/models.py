5eddd71d11db9f12b6ef6762aa25dc8b99e801ed
David Alejandro Reyes Milian <davidreyesmilian91@gmail.com>
2021-10-18 17:47:48 +0200

    fix flake stuff

--- app/core/models.py
+++ app/core/models.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.db import models
-from django.db.models import CASCADE
 
 
 class UserManager(BaseUserManager):
--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -2,7 +2,6 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from recipe import views
-from user.views import CreateUserView, CreateTokenView, ManageUserView
 
 app_name = 'recipe'
 
