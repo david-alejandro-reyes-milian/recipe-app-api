3adfee2e010e82b870e2218af31cd95a615d5d1a
David Alejandro Reyes Milian <davidreyesmilian91@gmail.com>
2021-10-18 20:57:19 +0200

    create ingredient api

---
+++ app/recipe/tests/test_ingredients_api.py
@@ -0,0 +1,54 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from core.models import Tag, Ingredient
+from recipe.serializers import TagSerializer, IngredientSerializer
+
+INGREDIENT_URL = reverse('recipe:ingredient-list')
+
+
+class PublicIngredientApiTests(TestCase):
+    def setUp(self) -> None:
+        self.client = APIClient()
+
+    def test_login_required(self):
+        res = self.client.get(INGREDIENT_URL)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateIngredientApiTests(TestCase):
+    def setUp(self) -> None:
+        self.user = get_user_model().objects.create_user(
+            'a@gmmail.com',
+            'mypassword'
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(self.user)
+
+    def test_retrieve_ingredients_list(self):
+        Ingredient.objects.create(name="I1", user=self.user)
+
+        res = self.client.get(INGREDIENT_URL)
+
+        ingredients = Ingredient.objects.all().order_by('-name')
+        serializer = IngredientSerializer(ingredients, many=True)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_ingredients_limited_to_user(self):
+        user2 = get_user_model().objects.create_user(
+            'aa@gmmail.com',
+            'mypassword'
+        )
+        Ingredient.objects.create(name="I1", user=user2)
+        ingredient = Ingredient.objects.create(name="I2", user=self.user)
+
+        res = self.client.get(INGREDIENT_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.data), 1)
+        self.assertEqual(res.data[0]['name'], ingredient.name)
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Tag
+from core.models import Tag, Ingredient
 from recipe.serializers import TagSerializer
 
 
@@ -12,6 +12,22 @@ class TagViewSet(viewsets.GenericViewSet,
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
     queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+# noinspection DuplicatedCode
+class IngredientViewSet(viewsets.GenericViewSet,
+                        mixins.ListModelMixin,
+                        mixins.CreateModelMixin):
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    queryset = Ingredient.objects.all()
     serializer_class = TagSerializer
 
     def get_queryset(self):
