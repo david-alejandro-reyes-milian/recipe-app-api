bde19ae4dd5605288a2adf76da723431c26ea3b6
David Alejandro Reyes Milian <davidreyesmilian91@gmail.com>
2021-10-18 21:01:16 +0200

    correct serializar

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -3,7 +3,7 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
 from core.models import Tag, Ingredient
-from recipe.serializers import TagSerializer
+from recipe.serializers import TagSerializer, IngredientSerializer
 
 
 class TagViewSet(viewsets.GenericViewSet,
@@ -28,7 +28,7 @@ class IngredientViewSet(viewsets.GenericViewSet,
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
     queryset = Ingredient.objects.all()
-    serializer_class = TagSerializer
+    serializer_class = IngredientSerializer
 
     def get_queryset(self):
         return self.queryset.filter(user=self.request.user).order_by('-name')
