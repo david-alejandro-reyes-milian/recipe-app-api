cb244f3968d2a595b9586b2ea380e7286924db01
David Alejandro Reyes Milian <davidreyesmilian91@gmail.com>
2021-10-26 09:31:14 +0200

    enable pillow + media serving

--- app/app/settings.py
+++ app/app/settings.py
@@ -119,5 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+# noinspection PyUnresolvedReferences
+STATIC_ROOT = '/vol/web/static'
+
+MEDIA_URL = '/media/'
+# noinspection PyUnresolvedReferences
+MEDIA_ROOT = '/vol/web/media'
 
 AUTH_USER_MODEL = 'core.User'
